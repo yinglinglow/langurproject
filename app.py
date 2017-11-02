129a27a39bc0cbb6e3a927b455b0c5986536c1d1
yingling <21985915+yinglinglow@users.noreply.github.com>
2017-11-02 15:32:20 +0800

    flask

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
-# -*- coding: utf-8 -*-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from flask import Flask, render_template
 
-app = dash.Dash()
-server = app.server
+app = Flask(__name__)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
-])
+@app.route('/')
+def main():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-
+    app.run(debug=True)

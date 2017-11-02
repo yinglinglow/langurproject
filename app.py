25956d277d267217050438bee15fc3ec407e374b
yingling <21985915+yinglinglow@users.noreply.github.com>
2017-11-02 14:38:01 +0800

    test

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
 app = dash.Dash()
 server = app.server
 
-print('hello world!!!!!!')
+print('heya')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
